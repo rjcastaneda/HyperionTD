d00b1894d3f8d402333a7b04ddee1a251e82879d
Roswell James Castaneda <roswelljcastaneda@gmail.com>
2020-04-25 23:34:42 -0700

    Added: Main Menu Script

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -1,18 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Button startButton;
+    Button HTPButton;
+    Button exitButton;
+
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        startButton = GameObject.Find("StartButton").GetComponent<Button>();
+        exitButton = GameObject.Find("ExitButton").GetComponent<Button>();
+        HTPButton = GameObject.Find("HTPButton").GetComponent<Button>();
     }
 }
