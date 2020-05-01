0f215243d2b94582ae2c2148660a03a0381d1938
Roswell James Castaneda <roswelljcastaneda@gmail.com>
2020-05-01 15:42:50 -0700

    Added: InGameUI Script, Camera Movement

---
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    private Vector3 camPos;
+    private float zoom;
+
+    public float panSpeed = 30f;
+    public float camBorder = 10f;
+    public float zoomSpeed = 1f;
+
+    //Camera Clamp Boundaries
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+    public float minZ;
+    public float maxZ;
+
+    void Update()
+    {
+        //Directional Camera Movement
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector3(0, 0, panSpeed * Time.deltaTime), Space.World);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector3(0, 0 , -panSpeed * Time.deltaTime), Space.World);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector3(-panSpeed * Time.deltaTime, 0, 0), Space.World);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector3(panSpeed * Time.deltaTime, 0, 0), Space.World);
+        }
+
+    //ScrollWheel Camera Zoom
+        camPos = transform.position;
+        zoom = Input.GetAxis("Mouse ScrollWheel");
+        camPos.y -= zoom * 500 * zoomSpeed * Time.deltaTime;
+        transform.position = camPos;
+    }
+
+    void LateUpdate()
+    {
+        //Camera Bounds
+            transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, minX, maxX),
+            Mathf.Clamp(transform.position.y, minY, maxY),
+            Mathf.Clamp(transform.position.z, minZ, maxZ));
+    }
+}
---
+++ Assets/Scripts/InGameUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InGameUI : MonoBehaviour
+{
+    private GameObject BPButtonOne;
+    private GameObject BuildPanel;
+
+    public void Start()
+    {
+        BPButtonOne = GameObject.Find("BPButtonEN");
+        BuildPanel = GameObject.Find("InGameUI").transform.Find("BuildPanel").gameObject;
+    }
+
+    public void BuildPanelEnable()
+    {
+        BuildPanel.SetActive(true);
+        BPButtonOne.SetActive(false);
+    }
+
+    public void BuildPanelDisable()
+    {
+        BuildPanel.SetActive(false);
+        BPButtonOne.SetActive(true);
+    }
+}
